500b534a39bc01598c4b95ecdfe15748a0e8f431
Eric Koehler <rikola2@gmail.com>
2022-07-04 02:35:26 -0400

    Improve admin site forms

--- polevaultcity/poles/admin.py
+++ polevaultcity/poles/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from .models import Pole, Customer, Rental
+
 # Register your models here.
+admin.site.register(Pole)
+admin.site.register(Customer)
+admin.site.register(Rental)
--- polevaultcity/poles/models.py
+++ polevaultcity/poles/models.py
@@ -7,7 +7,7 @@ class Pole(models.Model):
     brand = models.CharField(max_length=100)
     inches = models.IntegerField()
     weight = models.IntegerField()
-    purchased_at = models.DateField()
+    purchased_at = models.DateField(blank=True, null=True)
 
     def is_rented(self):
         return False
@@ -15,6 +15,9 @@ class Pole(models.Model):
     def age(self):
         purchased = self.purchased_at.value_from_object(datetime.datetime)
         return datetime.date.today() - purchased
+
+    def __str__(self):
+        return f'{self.brand} - {self.inches} - {self.weight}'
 
 
 class Customer(models.Model):
@@ -25,15 +28,18 @@ class Customer(models.Model):
     def get_rentals(self):
         return
 
+    def __str__(self):
+        return self.name
+
 
 class Rental(models.Model):
     customer = models.ForeignKey(Customer, on_delete=models.DO_NOTHING)
     pole = models.ForeignKey(Pole, on_delete=models.DO_NOTHING)
-    checkout = models.DateTimeField()
-    due = models.DateTimeField()
-    returned = models.DateTimeField()
-    price = models.DecimalField(max_digits=10, decimal_places=2)
-    comment = models.TextField(max_length=300)
+    checkout = models.DateField()
+    due = models.DateField()
+    returned = models.DateField(blank=True, null=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2, blank=True, null=True)
+    comment = models.TextField(max_length=300, blank=True, null=True)
 
     def active(self) -> bool:
         return self.returned is None
@@ -42,4 +48,9 @@ class Rental(models.Model):
         if self.returned:
             return True
         return datetime.datetime.now() > self.due
-    
+
+    def __str__(self):
+        c = self.customer.name
+        p = self.pole.brand
+        l = self.pole.inches
+        return f"{c} - {p} - {l}"
