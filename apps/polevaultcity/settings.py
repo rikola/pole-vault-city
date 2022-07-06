321e0c61a657338e11adc7b18768130d4ef2dc06
Eric Koehler <rikola2@gmail.com>
2022-07-05 23:01:09 -0400

    Set ALLOWED_HOSTS to localhost and Heroku app URL

--- apps/polevaultcity/settings.py
+++ apps/polevaultcity/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['lovely-grand-canyon-95741.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
